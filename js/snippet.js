758dbfed79436cdcdec07c2e39b28dde8f3a0797
gliburch <sol@itemme.com>
2014-09-25 15:17:15 +0900

    snippet code copy button

--- js/snippet.js
+++ js/snippet.js
@@ -1,9 +1,34 @@
 $(function() {
   
   $('snippet').each(function() {
-    var snippet = $(this).html();
-    var snippet_decoded = $('<div/>').text(snippet).html();
-    $(this).after($('<pre/>').append($('<code/>').addClass('html').append($.trim(snippet_decoded))));
+    var obj = $(this);
+    var snippet_html = obj.html()
+    var snippet_html_decoded = $.trim($('<div/>').text(snippet_html).html());
+    // var code_block = $('<pre/>').append($('<code/>').addClass('html').append($.trim(snippet_decoded)));
+    // $(this).after(code_block);
+
+    // var code_block = $('<pre/>').append($('<code/>').addClass('html').append($.trim(snippet_decoded)));
+    
+    $('<div></div>')
+      .append($('<pre></pre>'))
+      .find('pre')
+        .append($('<code></code>'))
+        .addClass('html')
+        .find('code')
+          .append(snippet_html_decoded)
+        .end()
+      .end()
+      .append($('<button></button>'))
+      .find('button')
+        .addClass('btn btn-default btn-xs')
+        .text('Copy')
+        .on('click', function() {
+          prompt('[Ctrl-c] 를 눌러 복사하고, 원하는 곳에 [Ctrl-v] 를 눌러 붙여넣으세요.', snippet_html);
+        })
+      .end()
+      .insertAfter(obj);
+
+
   });
 
   $('pre').css({
