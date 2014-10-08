200c1f6bb5d87f1ac0d7c6eb5598923adecbf8c8
gliburch <studio.gliburch@gmail.com>
2014-10-08 19:25:05 +0900

    minor ui fix, "table with header" type add

--- assets/admin/ui_admin.js
+++ assets/admin/ui_admin.js
@@ -1,13 +1,19 @@
 var component = {
 	tabWithDropdown: function() {
-		$(document).on('click', 'ul.nav li a', function(e) {
-			e.preventDefault();
-			if($(this).attr('data-toggle') == 'dropdown') {
-			} else {
-				$('ul.nav li').removeClass('active');
+		$(document).on('click', 'ul.nav li a', function(event) {
+			event.preventDefault();
+			if($(this).attr('data-toggle') != 'dropdown') {
+				$(this).closest('ul.nav').find('li').removeClass('active');
 				$(this).closest('li').addClass('active');
 			}
 			$(this).parents('li')[1] ? $($(this).parents('li')[1]).addClass('active') : false;
+		});
+	},
+	buttonGroup: function() {
+		$(document).on('click', '.btn-group .btn', function(event) {
+			event.preventDefault();
+			$(this).closest('.btn-group').find('.btn').removeClass('active');
+			$(this).closest('.btn').addClass('active');
 		});
 	}
 }
@@ -15,5 +21,6 @@ var component = {
 $(function() {
 
 	component.tabWithDropdown();
+	component.buttonGroup();
 	
 });
